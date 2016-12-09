6237163c7a8a2496e76706fa025404ec11ada2de
shaune <contact@shaunewest.com>
2016-12-10 00:22:51 +1100

    convert to object array, add delete method

--- app.js
+++ app.js
@@ -24,19 +24,31 @@
      * @link https://toddmotto.com/digging-into-angulars-controller-as-syntax/
      * @link https://johnpapa.net/angularjss-controller-as-and-the-vm-variable/ 
      */
-    this.notes = [
-      'one',
-      'two',
-      'three',
-      'four'
-      ];
+    var notes = [
+      { title: 'one' },
+      { title: 'two' },
+      { title: 'three' }
+    ];
+
+    /* Get notes */
+    this.notes = notes;
 
     /**
      * @name Add note
      * @description Add a note to our notes 
      */
     this.addNote = function(note){
-      this.notes.push(note);
+      this.notes.push({
+        title: note
+      });
+    }
+
+    /**
+     * @name Delete note
+     * @description Add a note to our notes 
+     */
+    this.delNote = function(index){
+      this.notes.splice(index, 1);
     }
   };
 
