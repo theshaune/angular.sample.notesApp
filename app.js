4823f21799af856d5c5d75178d35bb73fc0a140d
shaune <contact@shaunewest.com>
2016-12-09 22:03:23 +1100

    change naming convention to this

--- app.js
+++ app.js
@@ -6,7 +6,7 @@
  */
 (function () {
   'use strict';
-  
+
   /* Angular Module */
   var app = angular.module('app',[]);
 
@@ -14,13 +14,22 @@
    * @ngdoc controller
    * @name NotesCtrl
    */
-  app.controller('NotesCtrl', ['$scope', function($scope) {
-    $scope.notes = [
+  app.controller('NotesCtrl', NotesCtrl);
+
+  /* Notes Controller function */
+  function NotesCtrl() {
+
+    /**
+     * Use this convention instead of $scope
+     * @link https://toddmotto.com/digging-into-angulars-controller-as-syntax/
+     * @link https://johnpapa.net/angularjss-controller-as-and-the-vm-variable/ 
+     */
+    this.notes = [
       'one',
       'two',
       'three',
       'four'
       ];
-  }]);
+  };
 
 })();
