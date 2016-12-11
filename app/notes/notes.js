97de722fb091983e9aba9f462cc7b82dcd20fb83
shaune <contact@shaunewest.com>
2016-12-11 12:52:42 +1100

    Initial
    Prototype based angular controller

--- app/notes/notes.js
+++ app/notes/notes.js
@@ -5,60 +5,37 @@
     .module('app.notes')
     .controller('Notes', Notes);
 
-  /* @ngInject */
+  Notes.$inject = ['$scope', 'dataservice'];
+
   function Notes($scope, dataservice) {
-  
-    /**
-     * Use this convention instead of $scope
-     * @link https://toddmotto.com/digging-into-angulars-controller-as-syntax/
-     * @link https://johnpapa.net/angularjss-controller-as-and-the-vm-variable/ 
-     */
-    this.notes = [];
-    this.getNotes = getNotes;
-    this.addNote  = addNote;
-    this.delNote  = delNote;
-
-    this.getNotes();
     
-    /** 
-     * @name Get Notes 
-     */
-    function getNotes() {
-      return dataservice.getNotes().then(function(data) {
-        this.notes = data;
-        return this.notes;
-      }.bind(this))
-    };
-
-    /**
-     * @name Add note
-     * @description Add a note to our notes 
-     */
-    function addNote(){
-      /* Check that there is a note to add from the newNote input */ 
-      if (!$scope.newNote)
-        return;
-
-      return dataservice.addNote({title: $scope.newNote}).then(function(data) {
-        this.notes = data;
-        
-        /* Clear newNote input model */
-        $scope.newNote = '';
-        
-        return this.notes;
-      }.bind(this));
-    };
-
-    /**
-     * @name Delete note
-     * @description Add a note to our notes 
-     */
-    function delNote(index){
-      return dataservice.delNote(index).then(function(data) {
-        this.notes = data;
-        return this.notes;
-      }.bind(this))
-    }
-
+    this.$scope = $scope;
+    this.notes  = [];
+    this.dataservice  = dataservice;
+    
+    this.getNotes();
   };
+
+  Notes.prototype.getNotes = function() {
+    return this.dataservice.getNotes().then((data) => {
+      this.notes = data;
+    });
+  };
+
+  Notes.prototype.addNote = function() {
+    if (!this.$scope.newNote)
+      return;
+
+    return this.dataservice.addNote({title: this.$scope.newNote}).then((data) => {
+      this.notes = data;
+      this.$scope.newNote = '';
+    })
+  };
+  
+  Notes.prototype.delNote = function(index) {
+    return this.dataservice.delNote(index).then((data) => {
+      this.notes = data;
+    });
+  };
+
 })();
